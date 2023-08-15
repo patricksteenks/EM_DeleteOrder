8ef2a7858944012a14b6aed128bbbd80abba3398
Patrick Steenks <patrick@keytoe.nl>
2023-08-15 10:24:57 +0200

    Update OrderController.php
    Updated code to make suitable for php 7.4

--- EM/DeleteOrder/controllers/Adminhtml/Sales/OrderController.php
+++ EM/DeleteOrder/controllers/Adminhtml/Sales/OrderController.php
@@ -35,76 +35,75 @@ class EM_DeleteOrder_Adminhtml_Sales_OrderController extends Mage_Adminhtml_Sale
 		$table_sales_flat_order_item= Mage::getSingleton('core/resource')->getTableName('sales_flat_order_item');
 		$table_sales_flat_order_payment= Mage::getSingleton('core/resource')->getTableName('sales_flat_order_payment');
 		$table_sales_flat_order_status_history= Mage::getSingleton('core/resource')->getTableName('sales_flat_order_status_history');					
-		$table_sales_flat_order_grid= Mage::getSingleton('core/resource')->getTableName('sales_flat_order_grid');
-		$table_log_quote= Mage::getSingleton('core/resource')->getTableName('log_quote');
-		$table_sales_order_tax= Mage::getSingleton('core/resource')->getTableName('sales_order_tax');
+		$table_sales_flat_order_grid= Mage::getSingleton('core/resource')->getTableName('sales_flat_order_grid');						
+		$table_log_quote= Mage::getSingleton('core/resource')->getTableName('log_quote');				
         $quoteId='';		
 		if (!empty($orderIds)) {
             foreach ($orderIds as $orderId) {
 					$query=null;
 					$order = Mage::getModel('sales/order')->load($orderId);					
-					if($order->increment_id){
-						/*$query="show tables like 'sales_flat_order'";
-						$rs=$write->fetchAll($query);*/						
+					if($order->increment_id){				
 						$incId=$order->increment_id;
 						if(in_array($table_sales_flat_order,$rsc_table)){
-							$query='SELECT entity_id   FROM  '.$table_sales_flat_order.'    WHERE increment_id="'.mysql_escape_string($incId).'"';
-							
-							$rs=$write->fetchAll($query);												
+							$query = 'SELECT entity_id FROM ' . $table_sales_flat_order . ' WHERE increment_id = :incId';
+                            $params = [':incId' => $incId];
+                            $rs = $write->fetchAll($query, $params);											
 						
-							$query='SELECT quote_id    FROM   '.$table_sales_flat_order.'        WHERE entity_id="'.mysql_escape_string($orderId).'"';
-							$rs1=$write->fetchAll($query);
+							$query = 'SELECT quote_id FROM ' . $table_sales_flat_order . ' WHERE entity_id = :orderId';
+                            $params = [':orderId' => $orderId];
+                            $rs1 = $write->fetchAll($query, $params);
 							$quoteId=$rs1[0]['quote_id'];							
 						}		
 						
 						$query='SET FOREIGN_KEY_CHECKS=1';
 						$rs3=$write->query($query);
-						//print_r($rsc_table);
-						//echo $table_sales_flat_creditmemo_comment;
 						if(in_array($table_sales_flat_creditmemo_comment,$rsc_table)){
-						//echo "DELETE FROM ".$table_sales_flat_creditmemo_comment."    WHERE parent_id IN (SELECT entity_id FROM ".$table_sales_flat_creditmemo." WHERE order_id=".$orderId.")";
-						//die;
-						$write->query("DELETE FROM ".$table_sales_flat_creditmemo_comment."    WHERE parent_id IN (SELECT entity_id FROM ".$table_sales_flat_creditmemo." WHERE order_id='".mysql_escape_string($orderId)."')");
-						}
-						//die;
-						
-						
-						if(in_array('sales_flat_creditmemo_item',$rsc_table)){
-						$write->query("DELETE FROM ".$table_sales_flat_creditmemo_item."       WHERE parent_id IN (SELECT entity_id FROM ".$table_sales_flat_creditmemo." WHERE order_id='".mysql_escape_string($orderId)."')");
+                            $query = "DELETE FROM $table_sales_flat_creditmemo_comment WHERE parent_id IN (SELECT entity_id FROM $table_sales_flat_creditmemo WHERE order_id = :orderId)";
+                            $params = [':orderId' => $orderId];
+                            $write->query($query, $params);                            
 						}
 						
+						if (in_array('sales_flat_creditmemo_item', $rsc_table)) {
+                            $query = "DELETE FROM $table_sales_flat_creditmemo_item WHERE parent_id IN (SELECT entity_id FROM $table_sales_flat_creditmemo WHERE order_id = :orderId)";
+                            $params = [':orderId' => $orderId];
+                            $write->query($query, $params);
+                        }                        
 						
-						if(in_array($table_sales_flat_creditmemo,$rsc_table)){
-						$write->query("DELETE FROM ".$table_sales_flat_creditmemo."            WHERE order_id='".mysql_escape_string($orderId)."'");
-						}
+						if (in_array($table_sales_flat_creditmemo, $rsc_table)) {
+                            $query = "DELETE FROM $table_sales_flat_creditmemo WHERE order_id = :orderId";
+                            $params = [':orderId' => $orderId];
+                            $write->query($query, $params);
+                        }                        
 						
+						if (in_array($table_sales_flat_creditmemo_grid, $rsc_table)) {
+                            $query = "DELETE FROM $table_sales_flat_creditmemo_grid WHERE order_id = :orderId";
+                            $params = [':orderId' => $orderId];
+                            $write->query($query, $params);
+                        }                        
 						
+						if (in_array($table_sales_flat_invoice_comment, $rsc_table)) {
+                            $query = "DELETE FROM $table_sales_flat_invoice_comment WHERE parent_id IN (SELECT entity_id FROM $table_sales_flat_invoice WHERE order_id = :orderId)";
+                            $params = [':orderId' => $orderId];
+                            $write->query($query, $params);
+                        }                        
 						
-						if(in_array($table_sales_flat_creditmemo_grid,$rsc_table)){
-						$write->query("DELETE FROM ".$table_sales_flat_creditmemo_grid."        WHERE order_id='".mysql_escape_string($orderId)."'");
-						}
+						if (in_array($table_sales_flat_invoice_item, $rsc_table)) {
+                            $query = "DELETE FROM $table_sales_flat_invoice_item WHERE parent_id IN (SELECT entity_id FROM $table_sales_flat_invoice WHERE order_id = :orderId)";
+                            $params = [':orderId' => $orderId];
+                            $write->query($query, $params);
+                        }
+                        
+                        if (in_array($table_sales_flat_invoice, $rsc_table)) {
+                            $query = "DELETE FROM $table_sales_flat_invoice WHERE order_id = :orderId";
+                            $params = [':orderId' => $orderId];
+                            $write->query($query, $params);
+                        }                        
 						
-						
-						if(in_array($table_sales_flat_invoice_comment,$rsc_table)){
-						
-						$write->query("DELETE FROM ".$table_sales_flat_invoice_comment." WHERE parent_id IN (SELECT entity_id FROM ".$table_sales_flat_invoice." WHERE order_id='".mysql_escape_string($orderId)."')");
-						}
-						
-						if(in_array($table_sales_flat_invoice_item,$rsc_table)){
-						
-						$write->query("DELETE FROM ".$table_sales_flat_invoice_item."     WHERE parent_id IN (SELECT entity_id FROM ".$table_sales_flat_invoice." WHERE order_id='".mysql_escape_string($orderId)."')");
-						}
-						
-						
-						if(in_array($table_sales_flat_invoice,$rsc_table)){
-						
-						$write->query("DELETE FROM ".$table_sales_flat_invoice."         WHERE order_id='".mysql_escape_string($orderId)."'");
-						}
-						
-						if(in_array($table_sales_flat_invoice_grid,$rsc_table)){
-						
-						$write->query("DELETE FROM ".$table_sales_flat_invoice_grid."     WHERE order_id='".mysql_escape_string($orderId)."'");
-						}	
+						if (in_array($table_sales_flat_invoice_grid, $rsc_table)) {
+                            $query = "DELETE FROM $table_sales_flat_invoice_grid WHERE order_id = :orderId";
+                            $params = [':orderId' => $orderId];
+                            $write->query($query, $params);
+                        }                        
 						
 						if($quoteId){						
 							if(in_array($table_sales_flat_quote_address_item,$rsc_table)){							
@@ -136,71 +135,90 @@ class EM_DeleteOrder_Adminhtml_Sales_OrderController extends Mage_Adminhtml_Sale
 							if(in_array('sales_flat_quote_payment',$rsc_table)){
 							$write->query("DELETE FROM ".$table_sales_flat_quote_payment."         WHERE quote_id=".$quoteId);
 							}
-							
 						}
 						
+						if (in_array($table_sales_flat_shipment_comment, $rsc_table)) {
+                            $query = "DELETE FROM $table_sales_flat_shipment_comment WHERE parent_id IN (SELECT entity_id FROM $table_sales_flat_shipment WHERE order_id = :orderId)";
+                            $params = [':orderId' => $orderId];
+                            $write->query($query, $params);
+                        }                        
 						
-						if(in_array($table_sales_flat_shipment_comment,$rsc_table)){
-						$write->query("DELETE FROM ".$table_sales_flat_shipment_comment."    WHERE parent_id IN (SELECT entity_id FROM ".$table_sales_flat_shipment." WHERE order_id='".mysql_escape_string($orderId)."')");
-						}
+						if (in_array($table_sales_flat_shipment_item, $rsc_table)) {
+                            $query = "DELETE FROM $table_sales_flat_shipment_item WHERE parent_id IN (SELECT entity_id FROM $table_sales_flat_shipment WHERE order_id = :orderId)";
+                            $params = [':orderId' => $orderId];
+                            $write->query($query, $params);
+                        }
+                        
+                        if (in_array($table_sales_flat_shipment_track, $rsc_table)) {
+                            $query = "DELETE FROM $table_sales_flat_shipment_track WHERE order_id IN (SELECT entity_id FROM $table_sales_flat_shipment WHERE order_id = :orderId)";
+                            $params = [':orderId' => $orderId];
+                            $write->query($query, $params);
+                        }                        
 						
-						if(in_array($table_sales_flat_shipment_item,$rsc_table)){
-						$write->query("DELETE FROM ".$table_sales_flat_shipment_item."         WHERE parent_id IN (SELECT entity_id FROM ".$table_sales_flat_shipment." WHERE order_id='".mysql_escape_string($orderId)."')");
-						}
+						if (in_array($table_sales_flat_shipment, $rsc_table)) {
+                            $query = "DELETE FROM $table_sales_flat_shipment WHERE order_id = :orderId";
+                            $params = [':orderId' => $orderId];
+                            $write->query($query, $params);
+                        }
+                        
+                        if (in_array($table_sales_flat_shipment_grid, $rsc_table)) {
+                            $query = "DELETE FROM $table_sales_flat_shipment_grid WHERE order_id = :orderId";
+                            $params = [':orderId' => $orderId];
+                            $write->query($query, $params);
+                        }                        
 						
+						if (in_array($table_sales_flat_order, $rsc_table)) {
+                            $query = "DELETE FROM $table_sales_flat_order WHERE entity_id = :orderId";
+                            $params = [':orderId' => $orderId];
+                            $write->query($query, $params);
+                        }
+                        
+                        if (in_array($table_sales_flat_order_address, $rsc_table)) {
+                            $query = "DELETE FROM $table_sales_flat_order_address WHERE parent_id = :orderId";
+                            $params = [':orderId' => $orderId];
+                            $write->query($query, $params);
+                        }                        
 						
-						if(in_array($table_sales_flat_shipment_track,$rsc_table)){						
-						$write->query("DELETE FROM ".$table_sales_flat_shipment_track."         WHERE order_id  IN (SELECT entity_id FROM ".$table_sales_flat_shipment." WHERE order_id='".mysql_escape_string($orderId)."')");
-						}
+						if (in_array($table_sales_flat_order_item, $rsc_table)) {
+                            $query = "DELETE FROM $table_sales_flat_order_item WHERE order_id = :orderId";
+                            $params = [':orderId' => $orderId];
+                            $write->query($query, $params);
+                        }
+                        
+                        if (in_array($table_sales_flat_order_payment, $rsc_table)) {
+                            $query = "DELETE FROM $table_sales_flat_order_payment WHERE parent_id = :orderId";
+                            $params = [':orderId' => $orderId];
+                            $write->query($query, $params);
+                        }
+                        
+						if (in_array($table_sales_flat_order_status_history, $rsc_table)) {
+                            $query = "DELETE FROM $table_sales_flat_order_status_history WHERE parent_id = :orderId";
+                            $params = [':orderId' => $orderId];
+                            $write->query($query, $params);
+                        }
+                        
+                        if ($incId && in_array($table_sales_flat_order_grid, $rsc_table)) {
+                            $query = "DELETE FROM $table_sales_flat_order_grid WHERE increment_id = :incId";
+                            $params = [':incId' => $incId];
+                            $write->query($query, $params);
+                        }                        
 						
-						
-						if(in_array($table_sales_flat_shipment,$rsc_table)){
-						
-						$write->query("DELETE FROM ".$table_sales_flat_shipment."             WHERE order_id='".mysql_escape_string($orderId)."'");
-						}
-						
-						
-						if(in_array($table_sales_flat_shipment_grid,$rsc_table)){
-						$write->query("DELETE FROM ".$table_sales_flat_shipment_grid."         WHERE order_id='".mysql_escape_string($orderId)."'");
-						}
-						
-						if(in_array($table_sales_flat_order,$rsc_table)){
-						$write->query("DELETE FROM ".$table_sales_flat_order."                     WHERE entity_id='".mysql_escape_string($orderId)."'");
-						}
-						
-						if(in_array($table_sales_flat_order_address,$rsc_table)){
-						$write->query("DELETE FROM ".$table_sales_flat_order_address."            WHERE parent_id='".mysql_escape_string($orderId)."'");
-						}
-						
-						if(in_array($table_sales_flat_order_item,$rsc_table)){						
-						$write->query("DELETE FROM ".$table_sales_flat_order_item."                 WHERE order_id='".mysql_escape_string($orderId)."'");
-						}
-						if(in_array($table_sales_flat_order_payment,$rsc_table)){
-						$write->query("DELETE FROM ".$table_sales_flat_order_payment."             WHERE parent_id='".mysql_escape_string($orderId)."'");
-						}
-						if(in_array($table_sales_flat_order_status_history,$rsc_table)){
-						$write->query("DELETE FROM ".$table_sales_flat_order_status_history."     WHERE parent_id='".mysql_escape_string($orderId)."'");
-						}
-						
-						if($incId&&in_array($table_sales_flat_order_grid,$rsc_table)){						
-							$write->query("DELETE FROM ".$table_sales_flat_order_grid."                 WHERE increment_id='".mysql_escape_string($incId)."'");
-	
-						}
-						if(in_array($table_sales_order_tax,$rsc_table)){
-							$write->query("DELETE FROM ".$table_sales_order_tax."                 WHERE order_id='".mysql_escape_string($orderId)."'");
-						}
-						
-						$query="show tables like '%".$table_log_quote."'";
-						$rsc_table_l=$write->fetchCol($query);	
-						if($quoteId&&$rsc_table_l){						
-								$write->query("DELETE FROM ".$table_log_quote." WHERE quote_id=".$quoteId);							
-						}
-						$write->query("SET FOREIGN_KEY_CHECKS=1");						
+						$query = "SHOW TABLES LIKE :tableLogQuote";
+                        $params = [':tableLogQuote' => '%' . $table_log_quote . '%'];
+                        $rsc_table_l = $write->fetchCol($query, $params);
+
+                        if ($quoteId && $rsc_table_l) {
+                            $query = "DELETE FROM $table_log_quote WHERE quote_id = :quoteId";
+                            $params = [':quoteId' => $quoteId];
+                            $write->query($query, $params);
+                        }
+
+                        $write->query("SET FOREIGN_KEY_CHECKS=1");
 					}					
 			}	
-		$this->_getSession()->addSuccess($this->__('Order(s) deleted.'));
-		}else{
-		$this->_getSession()->addError($this->__('Order(s) error.'));
+		    $this->_getSession()->addSuccess($this->__('Order(s) deleted.'));
+		} else {
+		    $this->_getSession()->addError($this->__('Order(s) error.'));
 		}		
 		$this->_redirect('*/*/');		
     }
